26235c0352453a871ef507f7cbacb3baf7aef116
Insiderflow <team@insiderflow.asia>
2025-09-26 01:27:19 +0800

    Fix RegistrationForm - add back useRouter import

--- web/src/components/RegistrationForm.tsx
+++ web/src/components/RegistrationForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 interface RegistrationFormProps {
   next: string;
